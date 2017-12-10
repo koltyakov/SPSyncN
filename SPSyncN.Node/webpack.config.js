689223b81ede3bf55623bdde5e3e866dfd4f33c9
Andrew Koltyakov <koltyakov@gmail.com>
2017-12-10 17:04:36 +0300

    BundleAnalyzerPlugin

--- SPSyncN.Node/webpack.config.js
+++ SPSyncN.Node/webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: './tmp/SPSyncN.js',
@@ -18,9 +19,12 @@ module.exports = {
     }]
   },
   plugins: [
-    new UglifyJSPlugin(),
+    new UglifyJSPlugin({
+      parallel: true
+    }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
-    })
+    }),
+    new BundleAnalyzerPlugin()
   ]
 };
