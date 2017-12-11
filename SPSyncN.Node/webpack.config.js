e2fc49c4d8b2349792de5bd4d6dc054f411e7ef8
Andrew Koltyakov <koltyakov@gmail.com>
2017-12-11 16:01:26 +0300

    Newtonsoft.Json version

--- SPSyncN.Node/webpack.config.js
+++ SPSyncN.Node/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    new BundleAnalyzerPlugin()
+    })
+    // new BundleAnalyzerPlugin()
   ]
 };
